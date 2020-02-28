999abb176ea6311e9b6c4beb8ff5275c63eeaed7
Arjunkumar E <arjunae798@gmail.com>
2020-02-28 11:02:53 +0530

    first commit

---
+++ src/main/java/com/arjunae/BloodBankApps/BloodBankAppsApplication.java
@@ -0,0 +1,13 @@
+package com.arjunae.BloodBankApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjunae/BloodBankApps/BloodBankAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunae.BloodBankApps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
